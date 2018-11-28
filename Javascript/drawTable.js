9cb45618526c6551a9c59b5b239a1ba246ee1c8e
bmart392 <bmart392@gmail.com>
2018-11-28 14:11:31 -0500

    Added more buttons to view schedule page and added functionality

--- Javascript/drawTable.js
+++ Javascript/drawTable.js
@@ -1,3 +1,5 @@
+// TODO adjust table filling function to insert based on date and time rather than index
+
 function drawTable(){
     const param = getParameter();
     console.log(param);
@@ -18,18 +20,23 @@ function getParameter(){
 function createTableFromObject(scheduleObject){
     scheduleObject = {
         startDate : new Date("May 5, 2018"),
-        endDate : new Date("May 20, 2018"),
+        endDate : new Date("May 19, 2018"),
         startTime : 1000,
-        endTime : 1600,
+        endTime : 1100,
         deltaTime : 20,
-        timeSlots : [1,2,3,4]
+        timeSlots : ["open", "closed", "booked", "open", "closed", "booked", "open",
+            "closed", "booked", "open", "closed", "booked", "open", "closed", "booked"]
     };
     var table = document.getElementById("scheduleTable");
 
+    initWeekShown(scheduleObject);
     fillTimeColumn(table, scheduleObject);
     fillDateRow(table, scheduleObject);
+    fillTimeSlots(table, scheduleObject);
+    // TODO create function that actually schedules a meeting
+    // TODO add function logic to each button when it is made
 
-    
+
 
     
 }
@@ -49,8 +56,9 @@ function fillTimeColumn(htmlTable, schedule){
             }
         }
         var row = htmlTable.insertRow();
+        var cell = row.insertCell();
         const rowString = insertCharacter(currentTime.toString(),2,":");
-        row.innerHTML = rowString;
+        cell.innerHTML = rowString;
     }
 }
 
@@ -73,4 +81,86 @@ function generateDateString(startDate, index){
     const monthString = (currentDate.getMonth()+1).toString();
     const yearString = currentDate.getFullYear().toString();
     return monthString + "/" + dayString + "/" + yearString;
+}
+
+function fillTimeSlots(htmlTable, schedule){
+    const numRows = ((schedule.endTime-schedule.startTime)*6/10)/schedule.deltaTime;
+    const rowOffset = 2;
+    const colOffset = 1;
+    var k;
+    for(k = 0; k < schedule.timeSlots.length; k++){
+        const rowWithoutOffset = (k % numRows);
+        const row = rowWithoutOffset + rowOffset;
+        const col = ((k - rowWithoutOffset) / numRows) + colOffset;
+        const status = getTimeSlotStatus(schedule.timeSlots[k]);
+        var cell = htmlTable.rows[row].insertCell();
+
+        switch(status){
+            case "open" :
+
+                var btn = document.createElement('input');
+                btn.type = "button";
+                btn.value = "book now";
+                cell.appendChild(btn);
+                break;
+            case "closed" :
+
+                cell.innerHTML = status;
+                break;
+            case "booked" :
+                var para = document.createTextNode(getTimeSlotName());
+                cell.appendChild(para);
+
+                var div = document.createElement("div");
+                var codeField = document.createElement('input');
+                codeField.type = "text";
+                div.appendChild(codeField);
+
+                var cancelBtn = document.createElement('input');
+                cancelBtn.type = "button";
+                cancelBtn.value = "Cancel Meeting";
+                div.appendChild(cancelBtn);
+
+
+                cell.appendChild(div);
+                break;
+        }
+
+    }
+}
+
+function getTimeSlotStatus(timeSlot){
+    return timeSlot;
+}
+
+function getTimeSlotName(){
+    return "Joe";
+}
+
+function initWeekShown(schedule) {
+    editCurrentWeekShown(1);
+    console.log(schedule.startDate.getTime());
+    const millisStart = schedule.startDate.getTime();
+    const millisEnd = schedule.endDate.getTime();
+    const totalDays = (( millisEnd -millisStart)/(86400000));
+    const totalWeeks = Math.ceil(totalDays/5);
+    editTotalWeeksShown(totalWeeks);
+    updateWeekLabel()
+}
+function editCurrentWeekShown(newWeek){
+    document.getElementById("currentWeek").innerHTML = newWeek.toString();
+}
+
+function editTotalWeeksShown(totalWeek){
+    var paragraph = document.getElementById("totalWeeks");
+    console.log(totalWeek.toString())
+    paragraph.innerHTML = totalWeek;
+
+}
+
+function updateWeekLabel(){
+    const currentWeek = document.getElementById("currentWeek").innerHTML;
+    const totalWeeks = document.getElementById("totalWeeks").innerHTML;
+    document.getElementById("weekTag").innerHTML = ("Week " + currentWeek +
+        " of " + totalWeeks +" Shown Below");
 }
