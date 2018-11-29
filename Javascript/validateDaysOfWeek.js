77570e8068a33e02ea069aea1acc8f9dfc0e7d4a
bmart392 <bmart392@gmail.com>
2018-11-28 19:49:47 -0500

    added name to schedule when created

--- Javascript/validateDaysOfWeek.js
+++ Javascript/validateDaysOfWeek.js
@@ -1,44 +1,42 @@
 function validateDaysOfWeek(){
 
-    const startDateValue = new Date(document.createCalendar.startingDate.value);
-    const endingDateValue = new Date(document.createCalendar.endingDate.value);
+    let startDateValue = new Date(document.createCalendar.startingDate.value);
+    let endingDateValue = new Date(document.createCalendar.endingDate.value);
 
-    const startingTime = parseInt(document.createCalendar.startingTime.value);
-    const endingTime = parseInt(document.createCalendar.endingTime.value);
+    let startingTime = parseInt(document.createCalendar.startingTime.value);
+    let endingTime = parseInt(document.createCalendar.endingTime.value);
 
 
     if (startDateValue > endingDateValue){
         document.getElementById("errorString").innerHTML = "Ending date entered is before starting date";
-        console.log("incorrect date")
         return false;
     }
 
-    const condition1 = startingTime >= endingTime;
-    const condition2 = startDateValue.getTime() === endingDateValue.getTime() ;
+    let condition1 = startingTime >= endingTime;
+    let condition2 = startDateValue.getTime() === endingDateValue.getTime() ;
     if (condition1 && condition2){
         document.getElementById("errorString").innerHTML = "Ending time entered is before starting time";
-        console.log("incorrect time")
         return false;
     }
 
     if (startDateValue.getDay() === 5 || startDateValue.getDay() === 6 ||
         endingDateValue.getDay() === 5 || endingDateValue.getDay() === 6){
             document.getElementById("errorString").innerHTML = "Schedule cannot start or end on weekend";
-            console.log("incorrect day of the week")
             return false;
     }
 
-    const startDateValueString = document.createCalendar.startingDate.value;
-    const endingDateValueString = document.createCalendar.endingDate.value;
+    let startDateValueString = document.createCalendar.startingDate.value;
+    let endingDateValueString = document.createCalendar.endingDate.value;
 
 
-    const startingTimeString = document.createCalendar.startingTime.value;
-    const endingTimeString = document.createCalendar.endingTime.value;
+    let startingTimeString = document.createCalendar.startingTime.value;
+    let endingTimeString = document.createCalendar.endingTime.value;
 
-    const timeStep = document.createCalendar.meetingLength.value;
+    let timeStep = document.createCalendar.meetingLength.value;
+    let scheduleName = document.getElementById("scheduleName").value;
 
 
-    const sentObject = {
+    let sentObject = {
         "newSchedule"
     :
         [
@@ -46,14 +44,15 @@ function validateDaysOfWeek(){
             {"endDate": endingDateValueString},
             {"startTime": startingTimeString},
             {"endTIme": endingTimeString},
-            {"timeDelta": timeStep}
+            {"timeDelta": timeStep},
+            {"scheduleName": scheduleName}
         ]
     }
     ;
 
-    const url = 'https://jsonplaceholder.typicode.com/posts';
+    let url = 'https://jsonplaceholder.typicode.com/posts';
     $.post(url,JSON.stringify(sentObject),function(data,status){
-        const newScheduleID = data;
+        let newScheduleID = data;
 
         let redirectURL = "viewSchedule.html";
 
