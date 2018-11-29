9625c4022e2e929b3f1f6479f2006664296fdfbd
Andrew Gulotta <aj.gulotta@gmail.com>
2018-11-28 23:45:09 -0500

    Initial lambda function work

---
+++ src/main/java/lambda/CreateScheduleRequest.java
@@ -0,0 +1,28 @@
+package main.java.lambda;
+
+import java.sql.Date;
+
+public class CreateScheduleRequest {
+
+	String name;
+	int startTime;
+	int endTime;
+	int delta;
+	Date startDate;
+	Date endDate;
+	String secretCode;
+	
+	public CreateScheduleRequest(String n, int st, int et, int d, Date sd, Date ed) {
+		name = n;
+		startTime = st;
+		endTime = et;
+		delta = d;
+		startDate = sd;
+		endDate = ed;
+	}
+	
+	public String toString() {
+		return(name + ", " + startTime + ", " + endTime + ", " + delta + ", " + startDate + ", " + endDate);
+	}
+	
+}
